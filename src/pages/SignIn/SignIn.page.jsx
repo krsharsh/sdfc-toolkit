20f4568472bfdffa2a1fd252b341475ed6bf10df
Nikhil Sharma <nikhil.harsh.sharma@gmail.com>
2021-10-09 20:26:25 +0530

    feat: added sign in component

---
+++ src/pages/SignIn/SignIn.page.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import FormInput from '../../components/FormInput/FormInput.component';
+import CustomButton from '../../components/CustomButton/CustomButton.component';
+
+import './SignIn.styles.scss';
+
+const SignIn = () => {
+  const [userCredentials, setUserCredentials] = useState({
+    email: '',
+    password: '',
+  });
+
+  const { email, password } = userCredentials;
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setUserCredentials({
+      email: '',
+      password: '',
+    });
+  };
+
+  const handleChange = (event) => {
+    console.log('Working');
+    const { value, name } = event.target;
+    setUserCredentials({ ...userCredentials, [name]: value });
+    console.log(email);
+    console.log(password);
+  };
+
+  return (
+    <div className='sign-in'>
+      <h2>I already have an account</h2>
+      <span>Sign in with your email and password</span>
+
+      <form onSubmit={handleSubmit}>
+        <FormInput
+          name='email'
+          type='email'
+          value={email}
+          handleChange={handleChange}
+          label='Email'
+          required
+        />
+        <FormInput
+          name='password'
+          type='password'
+          value={password}
+          handleChange={handleChange}
+          label='Password'
+        />
+
+        <div className='buttons'>
+          <CustomButton type='submit' value='Submit Form'>
+            Sign In
+          </CustomButton>
+          <CustomButton
+            type='button'
+            // onClick={googleSignInStart}
+            isGoogleSignIn
+          >
+            Sign In With Google
+          </CustomButton>
+        </div>
+      </form>
+      <Link to='/signup' className='sign-up-direction'>
+        Don't have an account SIGN UP
+      </Link>
+    </div>
+  );
+};
+
+export default SignIn;
