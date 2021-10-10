62b26580d0b239f3764ecb9eeae1c5d8c8938386
Nikhil Sharma <nikhil.harsh.sharma@gmail.com>
2021-10-11 03:51:37 +0530

    fix: if signed in then redirects to home

--- src/App.js
+++ src/App.js
@@ -45,11 +45,11 @@ class App extends React.Component {
         <Header />
         <Switch>
           <Route exact path='/' component={Homepage} />
-          {/* <Route path='/signin' component={SignIn} /> */}
           <Route
+            exact
             path='/signin'
             render={() =>
-              this.state.currentUser ? <Redirect to='/' /> : <SignIn />
+              this.props.currentUser ? <Redirect to='/' /> : <SignIn />
             }
           />
           <Route path='/signup' component={SignUp} />
@@ -62,8 +62,12 @@ class App extends React.Component {
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser,
+});
+
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: (user) => dispatch(setCurrentUser(user)),
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
