ff3fa23bceff5d3d1af668e2546dfd2f1ca19eca
Nikhil Sharma <nikhil.harsh.sharma@gmail.com>
2021-10-12 05:58:06 +0530

    feat: added spinner while code is extracted

--- src/pages/Callback/Callback.page.jsx
+++ src/pages/Callback/Callback.page.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+
 import queryString from 'query-string';
+import Spinner from '../../components/Spinner/Spinner.component';
 
 const Callback = (props) => {
-  let queries = queryString.parse(props.location.search);
-  console.log(queries);
-  return <div></div>;
+  const [loading, setloading] = useState(true);
+  let history = useHistory();
+
+  useEffect(() => {
+    console.log('Working');
+    let queries = queryString.parse(props.location.search);
+    console.log(queries);
+    history.push('/dashboard');
+  });
+  return <div>{loading ? <Spinner /> : null}</div>;
 };
 
 export default Callback;
