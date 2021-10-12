3123e08ee22e0252f999a348278e0c63105a80a7
Nikhil Sharma <nikhil.harsh.sharma@gmail.com>
2021-10-12 15:59:57 +0530

    feat: dashboard contains ConnectSalesforce container

--- src/pages/Dashboard/Dashboard.page.jsx
+++ src/pages/Dashboard/Dashboard.page.jsx
@@ -1,35 +1,41 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
-import CustomButton from '../../components/CustomButton/CustomButton.component';
+import {
+  selectCurrentUser,
+  selectDevelopmentCode,
+  selectProductionCode,
+} from '../../redux/user/user.selectors';
+import { selectEnvironment } from '../../redux/environment/environment.selectors';
+import ConnectSalesforce from '../../components/ConnectSalesforce/ConnectSalesforce.component';
 
 import './Dashboard.styles.scss';
 
-const Dashboard = (props) => {
-  const handleProductionClick = () => {
-    const clientId =
-      '3MVG9n_HvETGhr3Ao6N8bdy6kmqtuRF4MQjDADKx7Ova9pMiPN3e6Wrt5N.5a8y409CqpLkY_B7YyaZEQPVNO';
-    const url = `https://login.salesforce.com/services/oauth2/authorize?client_id=${clientId}&redirect_uri=http://localhost:3000/callback&response_type=code`;
-    console.log(url);
-    window.location.href = url;
-  };
+const Dashboard = ({
+  currentUser,
+  production,
+  development,
+  selectEnvironment,
+}) => {
+  console.log(production);
+  console.log(development);
+  console.log(selectEnvironment);
   return (
     <div className='dashboard'>
       <div className='jumbotron'>
-        <div className='connect connect-production '>
-          <div className='connect-title'>
-            Connect Your Salesforce Production Account
-          </div>
-          <CustomButton onClick={handleProductionClick}>Connect</CustomButton>
-        </div>
-        <div className='connect connect-development '>
-          <div className='connect-title'>
-            Connect Your Salesforce Devlopment Account
-          </div>
-          <CustomButton>Connect</CustomButton>
-        </div>
+        {!production ? <ConnectSalesforce environment='login' /> : null}
+        {!development ? <ConnectSalesforce environment='test' /> : null}
       </div>
     </div>
   );
 };
 
-export default Dashboard;
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
+  production: selectProductionCode,
+  development: selectDevelopmentCode,
+  selectEnvironment: selectEnvironment,
+});
+
+export default connect(mapStateToProps)(Dashboard);
